11d51e4147e2955c75821061c11b6b50f54bda9d
Andrey Kulaga <kulagaad@gmail.com>
2022-09-18 16:42:22 +0300

    added @Repository annotation

--- src/main/java/com/example/yadoapi/repositories/ItemRepository.java
+++ src/main/java/com/example/yadoapi/repositories/ItemRepository.java
@@ -1,11 +1,10 @@
 package com.example.yadoapi.repositories;
 
 import com.example.yadoapi.models.SystemItemImport;
-import net.minidev.json.JSONArray;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<SystemItemImport, String> {
 
-//    void deleteFolder(SystemItemImport entity);
-//    JSONArray getInfoForFOlder(SystemItemImport entity);
 }
